e2155dbf4ef29947675b492599cb6e10b5021348
Dmytro Kovalov <dmitriykovalov@gmail.com>
2016-06-09 21:46:08 +0300

    b-4: fixes

--- src/main/java/com/dkovalov/movieland/dao/postgres/MovieDaoImpl.java
+++ src/main/java/com/dkovalov/movieland/dao/postgres/MovieDaoImpl.java
@@ -18,8 +18,8 @@ public class MovieDaoImpl implements MovieDao {
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final MovieRowMapper movieRowMapper = new MovieRowMapper();
     private static final String ORDER_BY_CLAUSE = " order by ";
-    private static final String RATING_COLUMN_NAME = "rating";
-    private static final String PRICE_COLUMN_NAME = "price";
+    private static final String RATING_COLUMN_NAME = ",rating ";
+    private static final String PRICE_COLUMN_NAME = ",price ";
     @Autowired
     private JdbcTemplate jdbcTemplate;
     @Value("${sql.movie.all}")
@@ -31,19 +31,10 @@ public class MovieDaoImpl implements MovieDao {
     public List<Movie> getAll(String ratingOrder, String priceOrder) {
         log.info("Start querying movies");
         long startTime = System.currentTimeMillis();
-        String orderClause = "";
-        if (ratingOrder != null || priceOrder != null) {
-            orderClause = ORDER_BY_CLAUSE;
-            if (ratingOrder != null) {
-                orderClause += RATING_COLUMN_NAME + " " + ratingOrder;
-                if (priceOrder != null) {
-                    orderClause += "," + PRICE_COLUMN_NAME + " " + priceOrder;
-                }
-            } else {
-                if (priceOrder != null) {
-                    orderClause += PRICE_COLUMN_NAME + " " + priceOrder;
-                }
-            }
+        String orderClause = (ratingOrder != null) ? (RATING_COLUMN_NAME + ratingOrder) : "";
+        orderClause += (priceOrder != null) ? (PRICE_COLUMN_NAME + priceOrder) : "";
+        if (!orderClause.equals("")) {
+            orderClause = ORDER_BY_CLAUSE + orderClause.substring(1);
         }
         List<Movie> movies = jdbcTemplate.query(fetchAllSQL + orderClause, movieRowMapper);
         log.info("Finish querying movies. Elapsed time - {} ms", System.currentTimeMillis() - startTime);
